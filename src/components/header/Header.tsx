5130868a572765272635afae77c2f965b90eaf1b
rodion121 <abnerjosefino@gmail.com>
2026-01-19 15:19:56 -0300

    sxs

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import './header.css';
+import Nexus from '../../assets/nexus.png';
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <header className="header">
+      <img src={Nexus} alt="Nexus Logo" />
+
+      <nav>
+        <Link to="/home" className="menu-item-link">Home</Link>
+        <Link to="/cadastro" className="menu-item-link">Cadastrar</Link>
+      </nav>
+    </header>
+  );
+}
