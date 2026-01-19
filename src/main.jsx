6641edcc6ed29544282dfca3223f1f7bd90893ce
rodion121 <abnerjosefino@gmail.com>
2026-01-19 14:54:49 -0300

    att

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './pages/home/home.jsx'
-import Registro from './pages/cadastro/registro.jsx'
-import LoginTw from './pages/loginTw/loginTw.jsx'
-import Teste from './pages/cadastro/teste.jsx'
 import App from './App.jsx'
-import Search from './pages/tabela/search.jsx'
-
 
 
 
